e38f988bfb853ec026c8f89e0024ab54b1a41ba8
JayMackay <jared.mackay1994@live.com>
2021-05-01 20:21:31 +0100

    Implemented Viewers Component

---
+++ src/components/Viewers.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Viewers() {
+    return (
+        <Container>
+            <wrap>
+                <img src="/images/viewers-disney.png" />
+            </wrap>
+            <wrap>
+                <img src="/images/viewers-pixar.png" />
+            </wrap>
+            <wrap>
+                <img src="/images/viewers-marvel.png" />
+            </wrap>
+            <wrap>
+                <img src="/images/viewers-starwars.png" />
+            </wrap>
+            <wrap>
+                <img src="/images/viewers-national.png" />
+            </wrap>
+        </Container>
+    )
+}
+
+export default Viewers
+
+const Container = styled.div`
+    margin-top: 30px;
+    display: grid;
+    padding: 30px 0px 26px;
+    grid-gap: 25px;
+    grid=template-column: repeat(5, minmax(0, 1fr));
+`
+
+const Wrap = styled.div`
+    border-radius: 10px;
+    cursor: pointer;
+    border: 3px solid rgba(249, 249, 249, 0.1);
+    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+    rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+
+    &:hover {
+        box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
+        rgb(0 0 0 / 72%) 0px 30px 10px -10px;
+        transform: scale(1.05);
+        border-color: rgba(249, 249, 249, 0.8);
+    }
+`
