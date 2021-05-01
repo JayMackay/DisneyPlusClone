cc1e53879cc7c9e0d10b13aa43d3a7b03db93e19
JayMackay <jared.mackay1994@live.com>
2021-05-01 22:00:10 +0100

    Minor Styling Changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Header from './components/Header'
 import Home from './components/Home'
 import Detail from '.components/Detail'
+import Login from '.components/Login'
 import {
   BrowserRouter as Router,
   Switch,
@@ -17,7 +18,10 @@ function App() {
     <Router>
       <Header />
       <Switch>
-        <Route path="detail">
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/detail">
           <Detail />
         </Route>
         <Route path="/">
